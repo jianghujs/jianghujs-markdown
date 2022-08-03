1a61341748cf55f77c93fb439ac306572ef7f21f
zenglh <1035337486@qq.com>
2022-08-03 14:10:41 +0800

    init

---
+++ app/service/constantUi.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// ========================================常用 require start===========================================
+const Service = require('egg').Service;
+const { tableEnum } = require('../constant/constant');
+// ========================================常用 require end=============================================
+
+
+class ConstantUiService extends Service {
+
+  async getConstantUiMap() {
+    const { jianghuKnex } = this.app;
+    const { pageId } = this.ctx.packagePage;
+    const { language } = this.app.config;
+    const constantUiList = await jianghuKnex(tableEnum._constant_ui).whereIn('pageId', ['all', pageId]).select();
+    const constantUiMap = Object.fromEntries(
+      constantUiList.map(obj => [obj.constantKey, JSON.parse(obj[language] || '{}')])
+    );
+    return constantUiMap;
+  }
+}
+
+module.exports = ConstantUiService;
---
+++ config/config.default.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const path = require("path");
+const assert = require("assert");
+
+const {
+  middleware,
+  middlewareMatch,
+} = require("@jianghujs/jianghu/config/middlewareConfig");
+
+const jianghuPathTemp = require.resolve('@jianghujs/jianghu');
+const jianghuPath = path.join(jianghuPathTemp, "../");
+
+module.exports = (appInfo) => {
+  assert(appInfo);
+
+  const appId = "admin";
+  const uploadDir = path.join(appInfo.baseDir, "upload");
+  const downloadBasePath = `/${appId}/upload`;
+
+  return {
+    appId,
+    appTitle: "openjianghu 文章管理",
+    appLogo: `${appId}/public/img/logo.png`,
+    appType: "single",
+    appDirectoryLink: "/",
+    indexPage: `/${appId}/page/articleManagement`,
+    adminIndex: `/${appId}/page/articleManagement`,
+    loginPage: `/${appId}/page/login`,
+    helpPage: `/${appId}/page/help`,
+    language: 'zh',
+    uploadDir,
+    uploadDirConfig: [
+      "/articleMaterial",
+      "/articleMaterial/_recycle",
+      "/materialRepo",
+      "/materialRepo/_recycle",
+      "/materialRepo/attachment",
+      "/materialRepo/image",
+      "/materialRepo/audio",
+      "/materialRepo/video",
+    ],
+    materialRepoDir: path.join(uploadDir, "materialRepo"),
+    articleMaterialDir: path.join(uploadDir, "articleMaterial"),
+    xfPageMaterialDir: path.join(uploadDir, "xfPageMaterial"),
+    downloadBasePath,
+    static: {
+      maxAge: 0,
+      buffer: false,
+      preload: false,
+      maxFiles: 0,
+      dir: [
+        {
+          prefix: `/${appId}/public/`,
+          dir: path.join(appInfo.baseDir, "app/public"),
+        },
+        {
+          prefix: `/${appId}/public/`,
+          dir: path.join(jianghuPath, "app/public"),
+        },
+      ],
+    },
+    view: {
+      defaultViewEngine: "nunjucks",
+      mapping: { ".html": "nunjucks" },
+      root: [
+        path.join(appInfo.baseDir, "app/view"),
+        path.join(jianghuPath, "app/view"),
+      ].join(","),
+    },
+    middleware,
+    ...middlewareMatch,
+    // 覆盖 downloadUserInfo，适配 /upload/ 开头的路由
+    downloadUserInfo: {
+      match(ctx) {
+        // url 格式符合 /appId/upload
+        return (ctx.request.method === 'GET' || ctx.request.method === 'HEAD')
+          && (
+            ctx.request.path.startsWith(`/${ctx.app.config.appId}/upload/`)
+            || ctx.request.path.startsWith(`/upload/`));
+      },
+    },
+  };
+};
